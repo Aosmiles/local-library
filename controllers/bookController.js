176ff434951ef537b8d4433764453184744e6b92
Aosmiles <arlin.smiles@gmail.com>
2022-11-03 16:59:16 -0700

    add some functionality to controllers

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,13 +1,47 @@
-// eslint-disable-next-line no-unused-vars
 import Book from "../models/book.js";
+import Author from "../models/author.js";
+import Genre from "../models/genre.js";
+import Bookinstance from "../models/bookinstance.js";
 
 export default {
   index: (req, res) => {
-    res.send("NOT IMPLEMENTED: Site Home Page");
+    Promise.all([
+      Book.countDocuments({}),
+      Bookinstance.countDocuments({}),
+      Bookinstance.countDocuments({ status: "Available" }),
+      Author.countDocuments({}),
+      Genre.countDocuments({}),
+    ])
+      .then((values) => {
+        const [books, bookinstances, bookinstancesAvailable, authors, genres] =
+          values;
+        res.render("home", {
+          title: "Local Library Home",
+          data: {
+            books,
+            bookinstances,
+            bookinstancesAvailable,
+            authors,
+            genres,
+          },
+        });
+      })
+      .catch((err) => console.log(err));
   },
 
-  book_list: (req, res) => {
-    res.send("not implemented: book list");
+  book_list: (req, res, next) => {
+    Book.find({}, "title author")
+      .sort({ title: 1 })
+      .populate("author")
+      .exec(function (err, bookList) {
+        if (err) {
+          return next(err);
+        }
+        res.render("book_list", {
+          title: "Book List",
+          bookList: bookList,
+        });
+      });
   },
 
   book_detail: (req, res) => {
--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -2,8 +2,22 @@
 import BookInstance from "../models/bookinstance.js";
 
 export default {
-  bookinstance_list: (req, res) => {
-    res.send("not implemented: bookinstance list");
+  bookinstance_list: (req, res, next) => {
+    BookInstance.find()
+      .populate("book")
+      .exec(function (err, bookinstanceList) {
+        if (err) {
+          return next(err);
+        }
+
+        bookinstanceList.sort((a, b) => {
+          return a.book.title.localeCompare(b.book.title);
+        });
+        res.render("bookinstance_list", {
+          title: "Book Instance List",
+          bookinstanceList: bookinstanceList,
+        });
+      });
   },
 
   bookinstance_detail: (req, res) => {
